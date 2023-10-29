c94eace07b0e360effd496830706eda1b5147380
Tesla_Tank <109467147+Tesla-SHT@users.noreply.github.com>
2023-10-29 17:45:04 +0800

    A basic version of Electron without Vue and MySQL
    You just need to download Electron

---
+++ index.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow, globalShortcut} = require('electron');
+const path = require('path');
+
+let mainWindow;
+let appIcon;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.setMenu(null);
+  mainWindow.loadFile('index.html');
+  globalShortcut.register('CommandOrControl+Shift+I', () => {
+    mainWindow.webContents.openDevTools();
+  }); 
+  globalShortcut.register('CommandOrControl+R', () => {
+    mainWindow.reload();
+  });
+
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+app.on('will-quit', () => {
+  // 在应用程序退出时注销全局快捷键
+  globalShortcut.unregisterAll();
+});
