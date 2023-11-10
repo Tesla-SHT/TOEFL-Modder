072752192a3c88250caa9bf92dcf831fa399487b
Tesla-SHT <2036230339@qq.com>
2023-11-10 09:28:38 +0800

    Update bookcover and word html

---
+++ frontend/js/words.js
@@ -0,0 +1,2 @@
+var receivedData = localStorage.getItem('WordData');
+console.log('Received Data:', receivedData);
--- renderer.js
+++ renderer.js
@@ -6,30 +6,41 @@ const bookContent = document.getElementById('book-content');
 
 // 示例词书数据（可以从后端加载或其他方式获取）
 const books = [
-    { name: 'Book 1', filePath: '1.xlsx' },
-    { name: 'Book 2', filePath: '1.xlsx' }
+    { name: 'Bio Words', filePath: '/words/Bio Vocabulary (179 Words).xlsx' },
+    { name: '1', filePath: '1.xlsx' },
+    { name: 'GRE Warm-up', filePath: 'words/GRE warm up (240 words).xlsx' },
+    { name: 'College Words', filePath: 'words/important words for college students lyas K. (274 Words).xlsx' },
+    { name: 'GRE', filePath: 'words/The GRE Quantitative Reasoning Test Arithmetic.xlsx' },
+    { name: 'TOEFL', filePath: 'words/TOEFL Vocabulary (1000 Words).xlsx' }
     // 添加更多词书...
 ];
 
 // 动态生成词书列表
+const booksData = {}; // 用于跟踪已导入的词书数据
 books.forEach(book => {
     const bookButtons = {}; // 用于跟踪已创建的按钮
-    const booksData = {}; // 用于跟踪已导入的词书数据
     if (!bookButtons[book.name]) {
         const bookButton = document.createElement('button');
         bookButton.textContent = book.name;
         bookButton.addEventListener('click', () => {
-            importBookData(book.filePath, book.name);
+            importBookData(book.name);
         });
         bookButtons[book.name] = bookButton; // 将按钮添加到跟踪对象
         bookshelf.appendChild(bookButton);
-        console.log(bookshelf);
 
         // 初始化每个词书的数据数组
         booksData[book.name] = [];
     }
 });
-function importBookData(filePath, bookName) {
+function importBookData(bookName) {
+    // 遍历数组
+    for (var i = 0; i < books.length; i++) {
+        if (books[i].name === bookName) {
+            // 找到匹配的name值，获取对应的filepath值
+            var filePath = myArray[i].filepath;
+            break;
+        }
+    }
     const workbook = new ExcelJS.Workbook();
 
     workbook.xlsx.readFile(filePath)
@@ -44,8 +55,9 @@ function importBookData(filePath, bookName) {
                 };
                 // 将数据添加到特定词书的数组中
                 booksData[bookName].push(entry);
-            });
-
+            }); 
+            localStorage.setItem('WordData', booksData);
+            window.location.href = 'frontend/html/words.html';
             // 此时 booksData[bookName] 包含了导入的单词数据
             console.log(`Data for ${bookName}:`, booksData[bookName]);
         })
