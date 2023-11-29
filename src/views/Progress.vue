86e6bd01634670dbdc632c6b4df303254579dd3d
Shuoyi <hushuoyi@westlake.edu.cn>
2023-11-29 21:32:30 +0800

    font-end progress udate

--- src/views/Progress.vue
+++ src/views/Progress.vue
@@ -1,5 +1,42 @@
 <script>
 </script>
 <template>
+    <n-card style="margin:5% 3%;width:94%">
+        <n-grid :cols="6">
+            <n-gi :span="6">
+                <h3 align="center">You Have Met TOEFL-Modder for _ Days!</h3>
+            </n-gi>
+        </n-grid>
+    </n-card>
+    <n-card style="margin:5% 3%;width:94%">
+        <n-grid :cols="17">
+            <n-gi :span="8">
+                <h3 align="center">Monthly Progress</h3>
+            </n-gi>
+            <n-gi :span="1">
+
+            </n-gi>
+            <n-gi :span="8">
+                <h3 align="center">Weekly Progress</h3>
+            </n-gi>
+        </n-grid>
+
+    </n-card>
+    <n-card style="margin:5% 3%;width:94%">
+        <n-grid :cols="17">
+            <n-gi :span="8">
+                <h3 align="center">Accuracy</h3>
+            </n-gi>
+            <n-gi :span="1">
+
+            </n-gi>
+            <n-gi :span="8">
+                <h3 >Total Study Time</h3>
+                <h3 >Total Study Day</h3>
+                <h3 >Continuous Study Day </h3>
+            </n-gi>
+        </n-grid>
+
+    </n-card>
 </template>
 <style></style>
