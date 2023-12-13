0dfdadbe2c0e907de8620fe43bbe62a73dbed8f6
Tesla-SHT <2036230339@qq.com>
2023-12-13 19:43:02 +0800

    Update QuickSetup.bat

--- QuickSetup.bat
+++ QuickSetup.bat
@@ -9,11 +9,12 @@ timeout /t 12
 start "" "%USERPROFILE%\Downloads\node-v20.10.0-x64.msi"
 pause
 cd /d %~dp0
-
-start /wait cmd /c "npm install"
+start /wait cmd /c "npm install -g cnpm --registry=https://registry.npm.taobao.org
+"
+start /wait cmd /c "cnpm install"
 echo.
 
 start cmd /c "npm run dev"
-timeout /t 3 >nul
+timeout /t 4 >nul
 
 start cmd /c "npm start"
