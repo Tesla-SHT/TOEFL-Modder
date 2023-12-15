6d6f63c23d5ba727c229ccdf554a8f519f6f7a4c
fswd-fswd <113104405+fswd-fswd@users.noreply.github.com>
2023-12-15 22:25:29 +0800

    recite

--- src/views/Recite.vue
+++ src/views/Recite.vue
@@ -507,14 +507,13 @@ export default {
     height: 80%;
     margin: 5% 5%;
     border-radius: 10px;
-    box-shadow: 0 0 10px #619163;
+    box-shadow: 0 0 4px #619163;
 }
 
 .WordCard {
     width: 90%;
     height: 80%;
     margin: 5% 5%;
-    margin-top: 2%;
     border-radius: 10px;
 
 }
